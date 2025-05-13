0c011caf20e027d5cdf23bd9e4d8a8c66aa0ec60
gus <angusedmunds1@gmail.com>
2025-05-13 19:09:37 +0100

    Add login with discord button

--- frontend-react/src/Login.tsx
+++ frontend-react/src/Login.tsx
@@ -5,16 +5,17 @@ import Box from '@mui/material/Box';
 
 function Login() {
   const navigate = useNavigate();
+  const oauthUrl = 'https://discord.com/oauth2/authorize?client_id=1371559638784278568&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A1337%2Fauth%2Fdiscord%2Fcallback&scope=identify';
 
   return (
     <>
       <h1>Login</h1>
       <Box
-      component="form"
-      sx={{ '& > :not(style)': { m: 1, width: '25ch' } }}
-      noValidate
-      autoComplete="off"
-    >
+        component="form"
+        sx={{ '& > :not(style)': { m: 1, width: '25ch' } }}
+        noValidate
+        autoComplete="off"
+      >
         <div>
           <TextField id="filled-basic" label="Username" variant="filled" />
         </div>
@@ -24,7 +25,23 @@ function Login() {
       </Box>
       
       <button onClick={() => navigate('/')}>Login</button>
-      {/* <button onClick={() => navigate('/')}>Go to Home</button> */}
+      
+      {/* Add the Login with Discord button */}
+      <button 
+        onClick={() => window.location.href = oauthUrl} 
+        style={{
+          marginTop: '20px',
+          padding: '10px 20px',
+          backgroundColor: '#5865F2',
+          color: 'white',
+          border: 'none',
+          borderRadius: '5px',
+          cursor: 'pointer',
+          fontSize: '16px'
+        }}
+      >
+        Login with Discord
+      </button>
     </>
   )
 }
